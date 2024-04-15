5d3223f4041b733b8fb34137b67859366025a500
SNguyen <84067158+SonNguyen25@users.noreply.github.com>
2024-04-15 02:10:52 -0400

    Update config.js

--- frontend/src/services/firebase/config.js
+++ frontend/src/services/firebase/config.js
@@ -4,13 +4,7 @@ import { getStorage, ref, uploadBytes, getDownloadURL } from 'firebase/storage';
 import { getFirestore} from 'firebase/firestore';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBpI0SsXA11jSRcfuuVqT5iOW_fH6g19vc",
-    authDomain: "ai4career.firebaseapp.com",
-    projectId: "ai4career",
-    storageBucket: "ai4career.appspot.com",
-    messagingSenderId: "985389595692",
-    appId: "1:985389595692:web:ae0b635c2b38dd93f7a589",
-    measurementId: "G-PERKJ7GFQG"
+    //Hidden
 };
 
 // Initialize Firebase
